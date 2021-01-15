40ba0cf21d99b67ca8ef1a77ca5d667d89e63658
tranttxuan <tran.ttxuan91@gmail.com>
2021-01-15 23:49:27 +0100

    modifiedannouncement model

--- models/Announcement.js
+++ models/Announcement.js
@@ -2,26 +2,27 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const AnnouncementSchema = new Schema({
-        service:{
-                childCare:Boolean,
-                seniorCare:Boolean,
-                petCare:Boolean
+        service: {
+                childCare: { type: Boolean, default: false },
+                seniorCare: { type: Boolean, default: false },
+                petCare: { type: Boolean, default: false },
         },
         additionalServices: {
-                houseKeeping: Boolean,
-                shoppingAndErrands: Boolean,
-                specialNeedsCare: Boolean,
-                homeworkAssistance: Boolean
+                houseKeeping: { type: Boolean, default: false },
+                shoppingAndErrands: { type: Boolean, default: false },
+                specialNeedsCare: { type: Boolean, default: false },
+                homeworkAssistance: { type: Boolean, default: false },
         },
+        title: String,
         description: String,
         formattedAddress: String,
         location: {
-          type: {type: String, enum: ["Point"],},
-          coordinates: {type: [Number]},
+                type: { type: String, enum: ["Point"], },
+                coordinates: { type: [Number] },
         },
-        time:String,
-        applicants: [{type: Schema.Types.ObjectId, ref:'User'}],
-        author:{type: Schema.Types.ObjectId, ref:'User', required:true}
+        time: String,
+        applicants: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+        author: { type: Schema.Types.ObjectId, ref: 'User', required: true }
 }, {
         timestamps: true
 });
