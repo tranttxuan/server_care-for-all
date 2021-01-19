58eb1ea3464c8ff203ecd6d5b029b6f437907dd4
tranttxuan <tran.ttxuan91@gmail.com>
2021-01-19 21:32:53 +0100

    populate data

--- routes/providers.js
+++ routes/providers.js
@@ -13,20 +13,16 @@ router.get('/', (req, res, next) => {
 // GET providers by service 
 router.get('/:service', (req, res, next) => {
         const service = req.params.service;
-        // *********how to use***************
-        // User.find({ `service.${req.params.service}`: true })
-        //         .then(list => {
-        //                 res.status(200).json(list)
-        //         })
-        //         .catch(err => res.status(400).json(err))
 
         if (service === "childCare") {
                 User.find({ $and: [{ "service.childCare": true }, { isProvider: true }] })
+                        .populate("reviews")
                         .then(list => { res.status(200).json(list) })
                         .catch(err => res.status(500).json(err))
                 return;
         } else if (service === "childCare") {
                 User.find({ $and: [{ "service.seniorCare": true }, { isProvider: true }] })
+                        .populate("reviews")
                         .then(list => {
                                 console.log(list)
                                 res.status(200).json(list)
@@ -35,6 +31,7 @@ router.get('/:service', (req, res, next) => {
                 return;
         } else {
                 User.find({ $and: [{ "service.petCare": true }, { isProvider: true }] })
+                        .populate("reviews")
                         .then(list => { res.status(200).json(list) })
                         .catch(err => res.status(500).json(err))
                 return;
