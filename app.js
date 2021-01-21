dcf89fd0d338a158de3807312613766636713896
tranttxuan <tran.ttxuan91@gmail.com>
2021-01-21 09:56:22 +0100

    build react

--- app.js
+++ app.js
@@ -46,6 +46,16 @@ app.use(function (req, res, next) {
 });
 
 /**
+ * Deploying to Heroku
+ */
+if (process.env.NODE_ENV === "production") {
+  app.use("*", (req, res, next) => {
+    // If no routes match, send them the React HTML.
+    res.sendFile(__dirname + "/public/index.html");
+  });
+}
+
+/**
  * Routes
  */
 
